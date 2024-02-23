606167c1989251de8ae51b230607407d0b55cbb5
TheMAN1003 <fi.03maksimnedozhdii@gmail.com>
2024-02-23 14:20:53 +0200

    Task 1: floating point and Unit tests for floating point functions

--- floating_point.h
+++ floating_point.h
@@ -1,6 +1,37 @@
 #ifndef MATRIX_MUL_FLOATING_POINT_H
 #define MATRIX_MUL_FLOATING_POINT_H
 
+// Multiplication of two matrices with any sizes using floating point.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define ASSERT_TRUE(condition) if (!(condition)) { printf("Failed assertion: %s\n", #condition); exit(1); }
+#define ASSERT_FALSE(condition) ASSERT_TRUE(!(condition))
+#define ASSERT_EQUAL(expected, actual) ASSERT_TRUE(expected == actual)
+#define ASSERT_NULL(ptr) ASSERT_TRUE(ptr == NULL)
+#define ASSERT_NOT_NULL(ptr) ASSERT_TRUE(ptr != NULL)
+
+
+extern float** createMatrix(uint32_t rows, uint32_t columns);
+extern void freeMatrix(float** matrix, uint32_t rows);
+
+extern bool isMultiplicable(uint32_t columns1, uint32_t rows2);
+extern bool floatEq(float a, float b, float prec);
+
+extern float** multiplyMatrices(float** matrix1, float** matrix2, uint32_t rows1, uint32_t columns1, uint32_t rows2, uint32_t columns2);
+
+
+
+
+
+
+
+
+
+
 
 
 #endif //MATRIX_MUL_FLOATING_POINT_H
